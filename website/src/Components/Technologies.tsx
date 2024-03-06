aebc7c225288188c159a36383f27e0ac1363b2bd
Rasmus <rasmustofthermansen@gmail.com>
2024-03-06 10:04:03 +0100

    Tech updated

--- website/src/Components/Technologies.tsx
+++ website/src/Components/Technologies.tsx
@@ -5,13 +5,17 @@ import Java from '../assets/java.png';
 import docker from '../assets/Docker.png';
 import python from '../assets/Python.png';
 import github from '../assets/Github.png';
+import sql from '../assets/sql-removebg-preview.png'
+import mongoDB from '../assets/mongodb-removebg-preview.png'
+import cassandra from  '../assets/cassandra-removebg-preview.png'
+import react from '../assets/react-removebg-preview.png';
 
 function Technologies() {
     return (
         <div className='container'>
             <div className='technologies'>
                 <div className='header'>
-                    <p>Tech I have been working with</p>
+                    <h2>Tech I have been working with</h2>
                 </div>
                 <div className='logos'>
                     <div className='logo-slide'>
@@ -35,6 +39,10 @@ const technologies = [
     { name: 'python', image: python },
     { name: 'Docker', image: docker },
     { name: 'github', image: github },
+    { name: 'SQL', image: sql },
+    { name: 'MongoDB', image: mongoDB},
+    { name: 'Cassandra', image: cassandra},
+    { name: 'React', image: react}
 ];
 
 export default Technologies;
