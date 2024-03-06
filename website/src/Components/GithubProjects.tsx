881c4d07acd248b6ef6b2041dea8abe01c13f56a
Rasmus <rasmustofthermansen@gmail.com>
2024-03-06 18:52:49 +0100

    Har lavet lidt på projects

---
+++ website/src/Components/GithubProjects.tsx
@@ -0,0 +1,36 @@
+// Projects.css
+import './Projects.css';
+import github from '../assets/Github.png'
+interface GitHubProject {
+  name: string;
+  link: string;
+  gameImage: string; 
+  
+}
+
+import React from 'react';
+import Github from '../assets/Github.png'; 
+
+interface Props {
+  project: GitHubProject;
+}
+
+const GitHubProjectComponent: React.FC<Props> = ({ project }) => {
+  return (
+    <div className='container'>
+      <div className='card-style'>
+        <div className='project-header'>
+          <h2>{project.name}</h2>
+        </div>
+        <p>
+          <a href={project.link} target="_blank" rel="noopener noreferrer">
+            <img src={project.gameImage} alt={project.name} width={300} height={250} />
+            <img src={github} alt="GitHub" width={150} height={75} /> 
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default GitHubProjectComponent;
--- website/src/Components/Projects.tsx
+++ website/src/Components/Projects.tsx
@@ -1,31 +1,60 @@
 import React from 'react';
-import './Projects.css'
+import './Projects.css';
+import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import GitHubProjectComponent from './GithubProjects';
+import mineswep from '../assets/Minesweeper.png'
+import snake from '../assets/Snake.png'
+
 function Projects() {
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3,
-          slidesToSlide: 3 
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2,
-          slidesToSlide: 2 
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1
-        }
-      };
-    return (
-      <div className='container'>
-        <div className='projects'>
-          
-        </div>
-     </div>
-    );
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+
+
+  const Minesweeper = {
+    name: 'Mine sweeper',
+    link: 'https://github.com/DARPZZ/Minesweeper',
+    gameImage: mineswep,
+
+  };
+  
+  const SnakeGame = {
+    name: "Snake game",
+    link: "https://github.com/DARPZZ/Snake-game",
+    gameImage:snake,
+  }
+
+
+  return (
+    <div className='container'>
+      <div className='project-header'>
+        <h2>Projects that I have made</h2>
+      </div>
+      <div className='caro'>
+        <Carousel responsive={responsive} swipeable={true} draggable={true} arrows={true}>
+          <GitHubProjectComponent project={Minesweeper} />
+          <GitHubProjectComponent project={SnakeGame}/>
+          <div>Item 3</div>
+          <div>Item 4</div>
+        </Carousel>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
