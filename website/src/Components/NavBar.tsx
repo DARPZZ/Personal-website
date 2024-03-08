d84975d77ef03e4dfe485093be1e992b5e652c74
Rasmus <rasmustofthermansen@gmail.com>
2024-03-08 07:19:09 +0100

    Updated navbar

--- website/src/Components/NavBar.tsx
+++ website/src/Components/NavBar.tsx
@@ -1,7 +1,33 @@
 import { HashRouter as Router, Link, Routes, Route } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
 import Home from './home';
 import './NavBar.css';
+
+import './home.css'
+
+import { HashLink as BaseHashLink } from 'react-router-hash-link';
+
+const HashLink = (props) => {
+  const { to, ...rest } = props;
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      const top = rect.top + window.scrollY - 50;
+      window.scrollTo({
+        top,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  return (
+    <BaseHashLink
+      {...rest}
+      to={to}
+      scroll={(el) => scrollTo(el.id)}
+    />
+  );
+};
 
 function NavBar() {
     const scrollToTop = () => {
@@ -17,6 +43,7 @@ function NavBar() {
                 <div className='routing'>
                     <div className='sticky-nav'>
                     <nav>
+                    <div className="animation"></div>
                         <ul>
                             <li>
                                 <HashLink smooth to="/#tech">Technologies</HashLink>
