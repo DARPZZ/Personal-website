61e2e603c9407ca474a56414bb6c63c8f68feb7e
Rasmus <rasmustofthermansen@gmail.com>
2024-03-18 15:06:22 +0100

    Changed contact

--- website/src/Components/Contact.tsx
+++ website/src/Components/Contact.tsx
@@ -10,16 +10,16 @@ function Contact() {
           <h2>You can get in contact with me here</h2>
         </div>
         <div className='links'>
-          <div className='linkedIn-inner'>
+         
             <a href={'https://www.linkedin.com/in/rasmus-toft-b880a827b/'} target="_blank" rel="noopener noreferrer">
               <img src={linkedInProfile} alt="LinkedIn" width={275} height={125} />
             </a>
-          </div>
-          <div className='Gmail-inner'>
+          
+          
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rasmustofthermansen@gmail.com" target="_blank" rel="noopener noreferrer">
               <img src={GmailLogo} alt="Gmail" width={150} height={100} />
             </a>
-          </div>
+          
 
         </div>
       </div>
