d9240c2a9a79f02fdcea2339b14dafa5453d26e6
Rasmus <rasmustofthermansen@gmail.com>
2024-03-29 03:38:31 +0100

    Convaterede over til ny slider for projects, dog mangler der stadig at lave ændringer

--- website/src/Components/Projects.tsx
+++ website/src/Components/Projects.tsx
@@ -1,116 +1,113 @@
 import React from 'react';
 import './Projects.css';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 import GitHubProjectComponent from './GithubProjects';
 import SpecialGitHubProjectComponent from './SpecialGitHubProjectComponent';
-import mineswep from '../assets/Minesweeper.png'
-import snake from '../assets/Snake.png'
-import Blackjack from '../assets/Blackjack.png'
-import nameguide from '..//assets/Name guide.png'
-import rockPaperSis from '../assets/rockpaper .png'
-import højrePil from '../assets/Højrepil.png'
-import venstrePil from '../assets/Venstrepil.png'
-const PreviousButton = ({ className = '', onClick }) => (
+import mineswep from '../assets/Minesweeper.png';
+import snake from '../assets/Snake.png';
+import blackjack from '../assets/Blackjack.png';
+import nameguide from '..//assets/Name guide.png';
+import rockPaperSis from '../assets/rockpaper .png';
+import højrePil from '../assets/Højrepil.png';
+import venstrePil from '../assets/Venstrepil.png';
+
+
+const PreviousButton = ({ onClick }: { onClick: any }) => (
   <div className='button1-div'>
-  <button id='test' className={`carousel-button-one ${className}`} onClick={onClick}>
-  <img src={venstrePil} width={40} height={40}/>
-  </button>
+    <button id='test' className='carousel-button-one' onClick={onClick}>
+      <img src={venstrePil} width={40} height={40} alt='Previous' />
+    </button>
   </div>
 );
 
-const NextButton = ({ className = '', onClick }) => (
-  <button className={`carousel-button-two ${className}`} onClick={onClick}>
-    <img src={højrePil} width={25} height={25}/>
-    
+const NextButton = ({ onClick }: { onClick: any }) => (
+  <button className='carousel-button-two' onClick={onClick}>
+    <img src={højrePil} width={25} height={25} alt='Next' />
   </button>
 );
 
-
-
-function Projects() {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1,
-    },
+const Projects = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: <PreviousButton onClick={() => {}} />,
+    nextArrow: <NextButton onClick={() => {}} />,
+    autoplay: true,
+    autoplaySpeed: 2000, 
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
-  const ButtonGroup = ({ next, previous, ...rest }) => {
-  const { carouselState: { currentSlide } } = rest;
-  return (
-    <div className="carousel-button-group">
-      <div className="left-button">
-        <PreviousButton className={currentSlide === 0 ? 'disable' : ''} onClick={() => previous()} />
-      </div>
-      <div className="right-button">
-        <NextButton onClick={() => next()} />
-      </div>
-    </div>
-  );
-};
-
 
   const Minesweeper = {
     name: 'Mine sweeper',
     link: 'https://github.com/DARPZZ/Minesweeper',
     info: 'minesweeper',
     gameImage: mineswep,
-
   };
-  
+
   const SnakeGame = {
-    name: "Snake game",
-    link: "https://github.com/DARPZZ/Snake-game",
-    gameImage:snake,
-  }
-  const blackjack ={
-    name: "Blackjack",
-    link: "https://github.com/DARPZZ/Blacjack-real",
+    name: 'Snake game',
+    link: 'https://github.com/DARPZZ/Snake-game',
+    gameImage: snake,
+  };
+
+  const Blackjack = {
+    name: 'Blackjack',
+    link: 'https://github.com/DARPZZ/Blackjack-real',
     info: 'blackjack',
-    gameImage: Blackjack,
-  }
-  const nameGuide ={
-    name: "Name guide",
-    link: "https://github.com/DARPZZ/Baby-names",
-    gameImage: nameguide
+    gameImage: blackjack,
+  };
 
-  }
-  const rockPaper={
-    name: "Rock paper",
-    link: "https://github.com/DARPZZ/Rock-paper-scissors",
+  const nameGuide = {
+    name: 'Name guide',
+    link: 'https://github.com/DARPZZ/Baby-names',
+    gameImage: nameguide,
+  };
+
+  const rockPaper = {
+    name: 'Rock paper',
+    link: 'https://github.com/DARPZZ/Rock-paper-scissors',
     gameImage: rockPaperSis,
-    playGame: "https://darpzz.github.io/Rock-paper-scissors/"
-
-  }
-
+    playGame: 'https://darpzz.github.io/Rock-paper-scissors/',
+  };
 
   return (
     <div className='container'>
       <div className='project-header'>
-        <h2>Projects  I have made</h2>
+        <h2>Projects I have made</h2>
       </div>
       <div className='caro'>
-        <Carousel responsive={responsive} customButtonGroup={<ButtonGroup next={NextButton} previous={PreviousButton} />} swipeable={true} draggable={true} arrows={false}>
+        <Slider {...settings}>
           <div><GitHubProjectComponent project={Minesweeper} /></div>
-           <div><GitHubProjectComponent project={SnakeGame}/></div>
-          <div> <GitHubProjectComponent project={blackjack}/></div>
-          <div><GitHubProjectComponent project={nameGuide}/></div>
-          <div><SpecialGitHubProjectComponent project={rockPaper}/></div>
-        </Carousel>
+          <div><GitHubProjectComponent project={SnakeGame} /></div>
+          <div><GitHubProjectComponent project={Blackjack} /></div>
+          <div><GitHubProjectComponent project={nameGuide} /></div>
+          <div><SpecialGitHubProjectComponent project={rockPaper} /></div>
+        </Slider>
       </div>
     </div>
   );
-}
+};
 
 export default Projects;
