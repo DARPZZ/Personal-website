1593efd3c68d32f7983d5e768ff28321b249cb76
Rasmus <rasmustofthermansen@gmail.com>
2024-03-30 01:36:39 +0100

    Updated minesweep info

--- website/src/Components/Info-Components/MinesweeperInfo.tsx
+++ website/src/Components/Info-Components/MinesweeperInfo.tsx
@@ -1,13 +1,16 @@
 import './Info.css'
+import React, { useEffect, useRef, useState } from 'react';
+import { scrollToTop } from '../Scroll';
 function MinesweeperInfo() {
+    scrollToTop()
     return (
        <div className='con'>
-        <div autoFocus={true}className='things-learned'>
+        <div className='things-learned'>
             <h1>Things that i have learned in this project</h1>
                 <div>
                     <h2>SDLC</h2>
-                    <h2>Recursion in c#</h2>
                     <h2>User interface</h2>
+                    <h2>Recursion in c#</h2>
                     <h2>Playing music in c#</h2>
                     <h2>Worked with share.cs</h2>
                     <h2>Game Condition(win and loose)</h2>
