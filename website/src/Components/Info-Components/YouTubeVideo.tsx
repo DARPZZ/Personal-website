0e4b57845f7a1f7929b73ce94658eb8a9b452c0a
Rasmus <rasmustofthermansen@gmail.com>
2024-04-03 12:22:07 +0200

    Added no cookie for youtube

--- website/src/Components/Info-Components/YouTubeVideo.tsx
+++ website/src/Components/Info-Components/YouTubeVideo.tsx
@@ -3,7 +3,7 @@ const YouTubeVideo = ({ videoId }) => (
     id="ifframe"
     width="100%" 
     height="650px"
-      src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`} 
+      src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&mute=1`} 
       title="YouTube Video Player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
       >
