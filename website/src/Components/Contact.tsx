16123706a37788b0ab21d03dba8e366512d14a22
Rasmus <rasmustofthermansen@gmail.com>
2024-04-12 07:43:41 +0200

    Made icons in contact more scaleable

--- website/src/Components/Contact.tsx
+++ website/src/Components/Contact.tsx
@@ -10,11 +10,11 @@ function Contact() {
           <h2>You can get in contact with me here</h2>
         </div>
         <div className='links'>
-            <a href={'https://www.linkedin.com/in/rasmus-toft-b880a827b/'} target="_blank" rel="noopener noreferrer">
-              <img src={linkedInProfile} alt="LinkedIn" width={275} height={125} />
+            <a  href={'https://www.linkedin.com/in/rasmus-toft-b880a827b/'} target="_blank" rel="noopener noreferrer">
+              <img id='linkedin-image' src={linkedInProfile} alt="LinkedIn"  />
             </a>
             <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rasmustofthermansen@gmail.com" target="_blank" rel="noopener noreferrer">
-              <img src={GmailLogo} alt="Gmail" width={150} height={100} />
+              <img id='gmail-image' src={GmailLogo} alt="Gmail"/>
             </a>
         </div>
       </div>
